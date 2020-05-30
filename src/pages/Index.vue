5de4c1905f0e993c72339db1e598eff36dc69daf
lukaskoeller <lukas-koeller@onlinehome.de>
2020-05-31 01:54:12 +0200

    Adding a graphql query example

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,6 +10,18 @@
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
     </p>
 
+    <div v-for="entry in $page.allStrapiService.edges" :key="entry.id">
+      <h2>{{ entry.node.name }}</h2>
+      <p>{{ entry.node.description }}</p>
+      <figure>
+        <img 
+          :src="`http://localhost:1337${entry.node.image.url}`" 
+          :alt="entry.node.image.caption" 
+          :title="entry.node.image.caption">
+        <figcaption>{{ entry.node.image.caption }}</figcaption>
+      </figure>
+    </div>
+
     <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
@@ -17,6 +29,24 @@
 
   </Layout>
 </template>
+
+<page-query>
+query {
+  allStrapiService {
+    edges {
+      node {
+          name
+          id
+          description
+          image {
+            url
+            caption
+          }
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
@@ -29,5 +59,13 @@ export default {
 <style>
 .home-links a {
   margin-right: 1rem;
+}
+
+figure {
+  margin: 0;
+}
+
+img {
+  max-width: 100%;
 }
 </style>
