452489ad01c208ab6647ce5c0cde7bcd2700b1aa
lukaskoeller <lukas-koeller@onlinehome.de>
2020-06-24 21:14:49 +0200

    add Card Component

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card">
+        <slot/>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+    display: inline-block;
+    border: var(--border-width) solid var(--basic-color-lighten-4);
+    padding: var(--spacing-10);
+}
+</style>
