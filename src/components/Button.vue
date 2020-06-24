95e10b3b9cfa07af234fd1c964e77ffbec1419a4
lukaskoeller <lukas-koeller@onlinehome.de>
2020-06-24 21:14:39 +0200

    Add Button Component Styling

---
+++ src/components/Button.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="button">
+        <slot/>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+//   data () {
+//     return {
+//       message: 'Try change me!'
+//     }
+//   },
+//   methods: {
+//     onClick () {
+//       this.message = 'Here you go :)'
+//     }
+//   }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+    display: inline-block;
+    border: var(--border);
+    padding: var(--spacing-2) var(--spacing-3);
+    
+    font-weight: bold;
+    text-transform: uppercase;
+    color: var(--secondary-color-darken-1);
+    letter-spacing: 0.5px;
+}
+</style>
