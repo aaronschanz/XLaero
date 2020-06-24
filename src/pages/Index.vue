6cecf2df0a44f4e42a4d0d884b261c9430a79bd5
lukaskoeller <lukas-koeller@onlinehome.de>
2020-06-24 21:15:37 +0200

    add some testing markup html

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,18 +9,31 @@
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
     </p>
-    <DefaultButton>Contact us</DefaultButton>
+    <Card>
+      <img width="600" src="https://images.unsplash.com/photo-1593004543863-3899599aec9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1974&q=80" alt="">
+      <h5>Hallo</h5>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      </p>
+      <Button>Contact us</Button>
+    </Card>
+    <h1>the brown fox jumps over the whatever h1</h1>
+    <h2>title big 2</h2>
+    <h3>title big 2</h3>
+    <h4>title big 2</h4>
+    <h5>title big 2</h5>
+    <h6>title big 2</h6>
 
-    <section>
+
+    <!-- potential strapi data
+      <section>
       <div class="post" v-for="post in $page.posts.edges" :key="post.node.id">
         <g-image class="post__img" :src="`http://localhost:1337${post.node.image.url}`" :title="post.node.image.caption" />
         <g-link :to="`/blog/${post.node.id}`" class="post__info">
-          <h2 class="post__title">{{ post.node.title }}</h2>
-          <h1 class="post__subtitle">{{ post.node.subtitle }}</h1>
           <div>{{ post.node.article }}</div>
         </g-link>
       </div>
-    </section>
+    </section> -->
 
     <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
@@ -30,34 +43,37 @@
   </Layout>
 </template>
 
-<page-query>
-query {
-  posts: allStrapiPost {
-    edges {
-      node {
-          id
-          title
-          subtitle
-          article
-          image {
-            url
-            caption
-          }
-      }
-    }
-  }
-}
-</page-query>
-
 <script>
-import DefaultButton from '~/components/DefaultButton.vue'
+// potential strapi query
+// <page-query>
+// query {
+//   posts: allStrapiPost {
+//     edges {
+//       node {
+//           id
+//           title
+//           subtitle
+//           article
+//           image {
+//             url
+//             caption
+//           }
+//       }
+//     }
+//   }
+// }
+// </page-query>
+
+import Button from '~/components/Button.vue'
+import Card from '~/components/Card.vue'
 
 export default {
   metaInfo: {
     title: 'Hello, world!'
   },
   components: {
-    DefaultButton
+    Button,
+    Card
   }
 }
 </script>
