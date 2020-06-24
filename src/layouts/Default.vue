13bd6da0634998895d7148da80a8db1f9b55b5af
lukaskoeller <lukas-koeller@onlinehome.de>
2020-06-24 21:15:26 +0200

    add body styling and wrap <slot> in <main>

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout">
+  <div>
     <header class="header">
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
@@ -9,7 +9,9 @@
         <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
     </header>
-    <slot/>
+    <main>
+      <slot/>
+    </main>
   </div>
 </template>
 
@@ -25,16 +27,9 @@ query {
 body {
   font-family: var(--font-family);
   background-color: var(--white-color);
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 .header {
