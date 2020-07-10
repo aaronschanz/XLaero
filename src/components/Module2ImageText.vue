248a5ff631c407768d2aee7a08bbe057ed78b4ed
lukaskoeller <lukas-koeller@onlinehome.de>
2020-07-10 23:03:00 +0200

    Add new Component module2imagetext

---
+++ src/components/Module2ImageText.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="container">
+        <div class="card-container">
+            <Card v-for="(container, i) in cards" :key="i" :modifiers="['card--no-border', 'card--full-size']">
+                    <img :src="container.img.src" :alt="container.img.caption">
+                    <div class="card__content">
+                        <h3>{{ container.headline }}</h3>
+                        <div v-html="container.text"></div>
+                        <Button :src="container.button.url">{{ container.button.label }}</Button>
+                    </div>
+            </Card>
+        </div>
+    </section>
+</template>
+
+<script>
+import Button from '~/components/Button.vue'
+import Card from '~/components/Card.vue'
+
+export default {
+    name: "Module2ImageText",
+    props: ['cards', 'modifiers'],
+    components: {
+        Button,
+        Card
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .card-container {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+    }
+</style>
