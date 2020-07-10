3dd4da9c74f91884f6220f9a4e2035ae9b94eb8b
lukaskoeller <lukas-koeller@onlinehome.de>
2020-07-10 23:02:23 +0200

    Add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <header class="header">
+            <g-link to="/" class="logo">
+                <g-image src="~/assets/images/xlaero-logo.svg" alt="XLaero aerospace engineering" />
+            </g-link>
+            <div class="menu">Menu</div>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+    .header {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: var(--spacing-10);
+
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: 1;
+    }
+
+    .logo {
+        display: block;
+        width: 220px;
+    }
+
+    .menu {
+        display: inline-block;
+        padding: var(--spacing-4) var(--spacing-6);
+        background-color: var(--secondary-color);
+        color: var(--white-color);
+        cursor: pointer;
+        // border-radius: var(--border-radius);
+    }
+</style>
