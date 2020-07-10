4f087c6907629d82adbb5a478302c7858838a86c
lukaskoeller <lukas-koeller@onlinehome.de>
2020-07-10 23:01:57 +0200

    Enable style modifiers for Card Component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="card">
+    <div :class="`card ${ modifiers.join(' ') }`">
         <slot/>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'Card'
+  name: 'Card',
+  props: {
+      modifiers: {
+          type: Array,
+          default: () => []
+      }
+  }
 }
 </script>
 
@@ -22,6 +28,23 @@ export default {
         height:  calc(32vw - var(--spacing-10) * 3);
         min-height: 360px;
         object-fit: cover;
+    }
+
+    &__content {
+        padding-left: var(--spacing-10);
+        padding-right: var(--spacing-10);
+    }
+
+    // equals .card--no-border
+    &--no-border {
+        border: 0;
+        padding: 0;
+    }
+
+    // equals .card--full-size
+    &--full-size {
+        width: 50%;
+        height: 100%;
     }
 }
 </style>
