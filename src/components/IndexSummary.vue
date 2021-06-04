d7641c26d961120c5df24c6ae0313ce87ec0a6c7
aaronschanz <56461665+aaronschanz@users.noreply.github.com>
2021-06-04 15:52:41 -0700

    Fixing spacing on homepage, updated indexbackground

--- src/components/IndexSummary.vue
+++ src/components/IndexSummary.vue
@@ -4,7 +4,7 @@
     <section class="container">
         <div class="center-container" :style="{ backgroundImage: `url(${require('~/../static/images/background-circle.svg')})`}">
             <g-image class="logo" src="~/../static/images/xlaero-logo-green.svg" alt="XLaero aerospace engineering" />
-            <h1>{{ content.headline }}</h1>
+            <!-- <h1>{{ content.headline }}</h1> -->
         </div>
         <div class ="summaries">
             <h3 id="capabilites">{{ content.text }}</h3>
@@ -12,13 +12,22 @@
             <h3 id="products">{{ content.text2 }}</h3>
             <h3 id="training">{{ content.text3 }}</h3>
         </div>
+        <div class="buttoncontainer">
+            <Button>Get in Touch</Button> <!-- Add src to contact us section -->
+        </div>
     </section>
 </template>
 
 <script>
+
+import Button from '~/components/Button.vue'
+
 export default {
     name: "ModuleHeadlineForm",
     props: ['content'],
+    components: {
+        Button
+    }
 }
 </script>
 
@@ -44,24 +53,34 @@ export default {
         }
     }
     .summaries {
+        margin-bottom: -120px;
+        margin-top: -400px;
         #capabilites {
-            position: absolute;
+            position: relative;
+            top: -350px;
             color: white;
+            text-align: left;
             //TODO
         }
         #research {
-            position: absolute;
+            position: relative;
             color: white;
+            top: -420px;
+            text-align: right;
             //TODO    
         }
         #products {
-            position: absolute;
+            position: relative;
             color: white;
+            top: -180px;
+            text-align: left;
             //TODO
         }
         #training {
-            position: absolute;
+            position: relative;
             color: white;
+            top: -250px;
+            text-align: right;
             //TODO
         }
     }
@@ -71,5 +90,13 @@ export default {
         height: 200px;
         margin-left: 60px;
         margin-bottom: 780px;
+    }
+    .buttoncontainer {
+        //margin-top: -100px;
+        width: max-content;
+        color: white;
+        position: relative;
+        top: 100px;
+        margin-bottom: -50px;
     }
 </style>
