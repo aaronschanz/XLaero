adb44c25b91371af2f81558d3fd9c0b8c087a9f9
aaronschanz <56461665+aaronschanz@users.noreply.github.com>
2021-06-08 15:46:21 -0700

    safety commit

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,7 +5,7 @@
             <div class="hero__content">
                 <div class="hero-headline">
                     <h1>XLaero - XL problem solving</h1>
-                    <p>20 years of bottom line improvement expertise gained from designing, certifying, supporting, and maintaining aircraft engines!</p>
+                    <p>20 years of bottom line improvement expertise gained from designing, certifying, supporting, and maintaining aircraft engines! **TO EDIT**</p>
                 </div>
                 <nav class="nav">
                     <div class="nav__item" v-for="(navItem, i) in navigation" :class="activeItem === i ? 'active' : ''" :style="activeItem === i ? { transform: `translateY(0%)` } : null" :key="i" @click="activeItem = i; changeText(i)" @mouseover="rotateItem = i">
